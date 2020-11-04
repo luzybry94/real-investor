15279f78ee6435fa46aad88d9736368d044fd3a6
luzybry94 <luzybry94@gmail.com>
2020-11-03 23:18:08 -0800

    add login routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    get '/login' do
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect '/properties'
+      else
+        redirect '/login'
+      end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
+end
+
